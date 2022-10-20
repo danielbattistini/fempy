f21d977e62d831b953b5be8d67f53093fa758117
danielbattistini <daniel.battistini@cern.ch>
2022-10-20 20:13:15 +0200

    add scripts for mj study

--- mult/CreateHMDataset.cc
+++ mult/CreateHMDataset.cc
@@ -1,40 +1,20 @@
-#include <TClonesArray.h>
-#include <TDatabasePDG.h>
-#include <TF1.h>
-#include <TFile.h>
-#include <TH1.h>
-#include <TH2.h>
-#include <TLorentzVector.h>
-#include <TMath.h>
-#include <TParticle.h>
-#include <TROOT.h>
-#include <TRandom3.h>
-#include <TStopwatch.h>
-#include <TSystem.h>
-
-#include <array>
-#include <deque>
-#include <map>
 #include <string>
 #include <vector>
 
+#include <TClonesArray.h>
+#include <TFile.h>
+#include <TStopwatch.h>
+
 #include "AliPythia8.h"
-#include "Math/GenVector/Boost.h"
-#include "Math/Vector3D.h"
-#include "Math/Vector4D.h"
 
 using namespace Pythia8;
 
 namespace {
 enum tunes { kMonash = 0, kCRMode0, kCRMode2, kCRMode3 };
-
 enum processes { kSoftQCD = 0, kHardQCD };
+enum triggers { kMB = 0, kHighMultV0, kHF};
 
 }  // namespace
-
-void StudyMultiplicity(int nEvents, float maxRunTime, tunes tune, processes process, int seed, std::string oDir);
-bool IsDetectable(int absPdg);
-
 
 bool IsDetectable(int absPdg) {
     if (absPdg == 11 ||   // electrons
@@ -48,13 +28,9 @@ bool IsDetectable(int absPdg) {
     return false;
 }
 
-void StudyMultiplicity(int nEvents, float maxRunTime, tunes tune, processes process, int seed, std::string oDir) {
+void CreateHMDataset(int nEvents, double maxRunTime, double energy, triggers trigger, tunes tune, processes process, int seed, std::string outDir) {
     TStopwatch timer;
     timer.Start();
-
-    int energy = 13000;
-    int collidingParticle1Pdg = 2212;
-    int collidingParticle2Pdg = 2212;
 
     //__________________________________________________________
     // create and configure pythia generator
@@ -136,40 +112,48 @@ void StudyMultiplicity(int nEvents, float maxRunTime, tunes tune, processes proc
     pythia.ReadString(Form("Random:seed %d", seed));
     pythia.Initialize(2212, 2212, energy);
 
-    // save root output file
-    std::string oFileName = "MultDistr";
-    if (collidingParticle1Pdg == 2212 && collidingParticle2Pdg == 2212) {
-        oFileName += "_pp";
-    } else {
-        printf("Error: colliding system not implemented! Exit!\n");
-        return;
-    }
-
+    std::string oFileName = "pythia_HMpp";
     if (energy == 13000) {
         oFileName += "13TeV";
     } else {
-        printf("Error: energy not implemented! Exit!\n");
+        printf("Error: energy not implemented. Exit!");
         return;
     }
 
+    if (tune == kMonash) {
+        oFileName += "_tune-Monash";
+    } else if (tune == kCRMode0) {
+        oFileName += "_tune-CRMode0";
+    } else if (tune == kCRMode2) {
+        oFileName += "_tune-CRMode2";
+    } else if (tune == kCRMode3) {
+        oFileName += "_tune-CRMode3";
+    } else {
+        printf("Error: tune not implemented. Exit!");
+        return;
+    }
+
+    if (process == kSoftQCD) {
+        oFileName += "_process-SoftQCD";
+    } else if (process == kHardQCD) {
+        oFileName += "_process-HardQCD";
+    } else {
+        printf("Error: process not implemented. Exit!");
+        return;
+    }
     oFileName += ".root";
+
     //__________________________________________________________
     // define outputs
-    TH2I *hMult = new TH2I("hMult", "hMult;particles in V0A or V0C;Partices in |#eta|<0.8;Entries", 201, -0.5, 200.5, 201, -0.5, 200.5);
-    TH2I *hEtaMultForward = new TH2I("hEtahEtaMultForward", "hEtaMultForward;#eta;Particles in V0A or V0C;Entries", 100, -4, 6, 201, -0.5, 200.5);
-    TH2I *hEtaMultMid = new TH2I("hEtahEtaMultMid", "hEtaMultMid;#eta;Particles in V0A or V0C;Entries", 100, -1, 1, 201, -0.5, 200.5);
-
     TClonesArray* particles = new TClonesArray("TParticle", 1000);
-
-    std::vector<float> etasForward;
-    std::vector<float> etasMid;
+    TTree* tEvents = new TTree("tEvents", "tEvents");
+    tEvents->Branch("particles", "TClonesArray", &particles);
 
     for (int iEvent = 0; iEvent < nEvents; iEvent++) {
-        printf("%f, %f\n", timer.RealTime(), maxRunTime*3600);
-        if (timer.RealTime() > maxRunTime*3600){
-            int time = int(timer.RealTime()); // in seconds
-            int nHours = int(time/3600);
-            int nMins = int((time%3600)/60);
+        if (timer.RealTime() > maxRunTime * 3600) {
+            int time = int(timer.RealTime());  // in seconds
+            int nHours = int(time / 3600);
+            int nMins = int((time % 3600) / 60);
 
             printf("Reached max run time: %d:%d\n", nHours, nMins);
             break;
@@ -177,43 +161,55 @@ void StudyMultiplicity(int nEvents, float maxRunTime, tunes tune, processes proc
             timer.Continue();
         }
 
-        etasForward.clear();
-        etasMid.clear();
-        
         pythia.GenerateEvent();
         pythia.ImportParticles(particles, "All");
 
-        // evaluate multiplicity at forward rapidity
-        int nChForward = 0;
-        int nChMid = 0;
-        for (auto iPart = 2; iPart < particles->GetEntriesFast(); ++iPart) {
-            TParticle* particle = dynamic_cast<TParticle*>(particles->At(iPart));
-            int pdg = std::abs(particle->GetPdgCode());
-            int status = std::abs(particle->GetStatusCode());
-            float eta = particle->Eta();
 
-            if (IsDetectable(pdg) && std::abs(eta) < 0.8 && status == 1) {
-                etasMid.push_back(eta);
-                nChMid++;
-            } else if (IsDetectable(pdg) && ((-3.7 < eta && eta < -1.7) || (2.8 < eta && eta < 5.1))  && status == 1) { // V0A and V0C acceptance
-                etasForward.push_back(eta);
-                nChForward++;
+        if (trigger == kMB) {
+            int nCh = 0;
+            for (auto iPart = 2; iPart < particles->GetEntries(); ++iPart) {
+                TParticle* particle = dynamic_cast<TParticle*>(particles->At(iPart));
+                int status = std::abs(particle->GetStatusCode());
+
+                if (status == 1) { // count particles in the final state
+                    nCh++;
+                }
             }
-        }
-        if (nChForward > 2 || nChMid > 2) {
-            for (float eta : etasForward) {
-                hEtaMultForward->Fill(eta, nChForward);
+            if (nCh > 2) { // reject diffractive events
+                tEvents->Fill();
             }
-            for (float eta : etasMid) {
-                hEtaMultMid->Fill(eta, nChMid);
+        } else if (trigger == kHighMultV0) {
+            // evaluate multiplicity at forward rapidity
+            int nChForward = 0;
+            for (auto iPart = 2; iPart < particles->GetEntriesFast(); ++iPart) {
+                TParticle* particle = dynamic_cast<TParticle*>(particles->At(iPart));
+                int pdg = std::abs(particle->GetPdgCode());
+                int status = std::abs(particle->GetStatusCode());
+                float eta = particle->Eta();
+
+                if (IsDetectable(pdg) && ((-3.7 < eta && eta < -1.7) || (2.8 < eta && eta < 5.1)) &&
+                    status == 1) {  // V0A and V0C acceptance
+                    nChForward++;
+                }
             }
-            hMult->Fill(nChForward, nChMid);
+            if (nChForward > 130) {
+                tEvents->Fill();
+            }
+        } else if (trigger == kHF) { // look for c or b quarks
+            for (auto iPart = 2; iPart < particles->GetEntriesFast(); ++iPart) {
+                TParticle* particle = dynamic_cast<TParticle*>(particles->At(iPart));
+                int pdg = std::abs(particle->GetPdgCode());
+
+                if (std::abs(pdg) == 4 || std::abs(pdg) == 5) {
+                    tEvents->Fill();
+                    break;
+                }
+            }
         }
     }
 
-    TFile outFile(Form("%s/%s", oDir.data(), oFileName.data()), "recreate");
-    hMult->Write();
-    hEtaMultForward->Write();
-    hEtaMultMid->Write();
+    // save root output file
+    TFile outFile(Form("%s/%s", outDir.data(), oFileName.data()), "recreate");
+    tEvents->Write();
     outFile.Close();
 }
