ee4ffaf6bc5b3ae5842543350d6f9cfa62f93aff
Daniel Battistini <daniel.battistini@cern.ch>
2022-11-09 16:21:11 +0100

    add option for ratio

--- CompareCFs.py
+++ CompareCFs.py
@@ -3,10 +3,10 @@ import sys
 import os
 import yaml
 
-from ROOT import TFile, TCanvas, TLegend
+from ROOT import TFile, TCanvas, TLegend, TLine
 
 from utils.io import GetObjectFromFile
-from utils.format import TranslateTolatex, FigInit
+from utils.format import TranslateToLatex, FigInit
 
 FigInit()
 
@@ -22,8 +22,15 @@ with open(args.cfg, "r") as stream:
         print(exc)
         sys.exit()
 
+doRatio = cfg['ratio']['enable']
 
-cPlot = TCanvas("cPlot", "cPlot", 600, 600)
+if doRatio:
+    cPlot = TCanvas("cPlot", "cPlot", 1200, 600)
+    cPlot.Divide(2, 1)
+    cPlot.cd(1)
+else:
+    cPlot = TCanvas("cPlot", "cPlot", 600, 600)
+
 inObjs = []
 legends = []
 for input in cfg["input"]:
@@ -34,6 +41,7 @@ for input in cfg["input"]:
         print(f'Error: cannot load {inFile}:{input["name"]}. Exit!')
         sys.exit()
 
+    inObj.Rebin(cfg['opt']['rebin'])
     inObj.SetDirectory(0)
     inObjs.append(inObj)
     legends.append(input['legend'])
@@ -48,6 +56,7 @@ for iObj, (inObj, legend) in enumerate(zip(inObjs, legends)):
     inObj.SetStats(0)
     inObj.SetLineWidth(2)
     if iObj == 0:
+        inObj.SetTitle(cfg['opt']['title'])
         inObj.GetXaxis().SetRangeUser(
             cfg['opt']['rangex'][0], cfg['opt']['rangex'][1])
         inObj.GetYaxis().SetRangeUser(
@@ -59,8 +68,36 @@ for iObj, (inObj, legend) in enumerate(zip(inObjs, legends)):
 leg.SetTextSize(0.03)
 leg.SetTextSize(0.03)
 
-leg.SetHeader(TranslateTolatex(cfg['opt']['leg']['header']), 'C')
+leg.SetHeader(TranslateToLatex(cfg['opt']['leg']['header']), 'C')
 leg.Draw()
+
+
+
+# ratio
+if doRatio:
+    cPlot.cd(2)
+
+    hDen = inObjs[0].Clone()
+    hDen.Rebin(cfg['ratio']['rebin'])
+    hRatio = inObjs[1].Clone()
+    hRatio.Rebin(cfg['ratio']['rebin'])
+    
+    hRatio.Sumw2()
+    
+    hRatio.Divide(hDen)
+
+
+    hRatio.SetTitle(cfg['opt']['title'])
+    hRatio.GetYaxis().SetTitle('Ratio')
+    hRatio.GetYaxis().SetRangeUser(cfg['ratio']['rangey'][0], cfg['ratio']['rangey'][1])
+    hRatio.GetXaxis().SetRangeUser(cfg['opt']['rangex'][0], cfg['opt']['rangex'][1])
+    hRatio.Draw()
+
+    line = TLine(cfg['opt']['rangex'][0], 1, cfg['opt']['rangex'][1], 1)
+    line.SetLineColor(13)
+    line.SetLineStyle(9)
+
+    line.Draw('same')
 
 for ext in cfg["opt"]["ext"]:
     cPlot.SaveAs(f'{os.path.splitext(args.oFileName)[0]}.{ext}')
