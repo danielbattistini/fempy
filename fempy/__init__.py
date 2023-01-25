586bf63508061bd48a7a8da72cf3ce7a1ef20fd6
Daniel Battistini <daniel.battistini@cern.ch>
2023-01-25 17:44:50 +0100

    add messages

--- fempy/__init__.py
+++ fempy/__init__.py
@@ -1,20 +1,44 @@
+import sys
+
 from ROOT import gROOT, gStyle
 
 from .correlation_function import CorrelationFunction
+from .utils import io
 
 gROOT.SetBatch(True)
 
+
 # set style
-# gStyle.SetOptStat(0)
-gStyle.SetOptTitle(0)
-gStyle.SetPalette(55)
-gStyle.SetLineWidth(2)
-gStyle.SetLegendBorderSize(0)
+def set_style(style):
+    gStyle.SetPalette(55)
+    gStyle.SetLineWidth(2)
+    gStyle.SetLegendBorderSize(0)
 
-gStyle.SetPadTickX(1)
-gStyle.SetPadTickY(1)
+    gStyle.SetPadTickX(1)
+    gStyle.SetPadTickY(1)
 
-gStyle.SetPadRightMargin(0.035)
-gStyle.SetPadLeftMargin(0.14)
-gStyle.SetPadTopMargin(0.035)
-gStyle.SetPadBottomMargin(0.1)
+    gStyle.SetPadRightMargin(0.035)
+    gStyle.SetPadLeftMargin(0.14)
+    gStyle.SetPadTopMargin(0.035)
+    gStyle.SetPadBottomMargin(0.1)
+    gStyle.SetOptTitle(0)
+
+    if style == 'title':
+        print('hehehe')
+        gStyle.SetOptTitle(1)
+        gStyle.SetPadTopMargin(0.1)
+
+    gROOT.ForceStyle()
+
+
+def error(msg):
+    print(f'\033[31mError:\033[0m {msg} ---> Exit!')
+    sys.exit()
+
+
+def warning(msg):
+    print(f'\033[33mWarning:\033[0m {msg}')
+
+
+def info(msg):
+    print(f'\033[34mInfo:\033[0m {msg}')
