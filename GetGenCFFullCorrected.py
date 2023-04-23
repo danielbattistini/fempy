7155551a2db53de112366b49182d5d32c135eaad
Daniel Battistini <daniel.battistini@cern.ch>
2023-04-23 12:56:13 +0200

    Fit gen CF

--- GetGenCFFullCorrected.py
+++ GetGenCFFullCorrected.py
@@ -3,7 +3,10 @@ import yaml
 
 import argparse
 
-from ROOT import TFile, TF1, TGraphErrors, TDatabasePDG
+from ROOT import TFile, TF1, TGraphErrors, TDatabasePDG, gInterpreter, TCanvas, kBlue, TLatex, gStyle, TLegend
+gInterpreter.ProcessLine('#include "fempy/utils/functions.h"')
+gInterpreter.ProcessLine('#include "fempy/MassFitter.hxx"')
+from ROOT import MassFitter, GeneralCoulombLednicky
 
 import fempy
 
@@ -93,12 +96,57 @@ def ApplyCenterOfGravity(hist, graph):
     return gCentered
 
 
+def WeightedCoulombLednicky(x, par):
+    # effective source radii
+    r1 = par[0]
+    r2 = par[1]
+
+    # source weights
+    w1 = par[2]
+    w2 = 1 - w1
+
+    scattLen = par[3]
+    effRange = par[4]
+
+    quantumStat = bool(par[5])
+    redMass = par[6]
+    chargeProd = par[7]
+
+    # coulomb + strong CF
+    gcl1 = GeneralCoulombLednicky(x[0], r1, scattLen, effRange, quantumStat, redMass, chargeProd)
+    gcl2 = GeneralCoulombLednicky(x[0], r2, scattLen, effRange, quantumStat, redMass, chargeProd)
+
+    return w1 * gcl1 + w2 * gcl2
+
+
+def RedMass(m1, m2):
+    return m1 * m2 / (m1 + m2)
+
+
+def MakeFlatHist(hSkeleton, value, name='hFlat'):
+    hFlat = hSkeleton.Clone(name)
+    hFlat.Reset()
+    nBins = hFlat.GetNbinsX()
+
+    # Set underflow and overflow to zero
+    hFlat.SetBinContent(0, 0)
+    hFlat.SetBinContent(nBins+1, 0)
+
+    for iBin in range(nBins):
+        hFlat.SetBinContent(iBin+1, value)
+        hFlat.SetBinError(iBin+1, 0)
+
+    return hFlat
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pair', choices=('DstarPi', 'DstarK'))
     parser.add_argument('--syst', action='store_true', default=False)
     parser.add_argument('--stat', action='store_true', default=False)
     args = parser.parse_args()
+
+    gStyle.SetOptStat(0)
 
     kStarBW = 50  # MeV/c
     if args.pair == 'DstarK':
@@ -108,7 +156,7 @@ if __name__ == '__main__':
         oFileName = f'/home/daniel/an/DstarK/2_luuksel/GenCFCorr_nopc_kStarBW50MeV_fromq.root'
         config = '/home/daniel/an/DstarK/cfg_gencf_DstarK_50MeV.yml'
 
-        lpdg = 321
+        lightMass = TDatabasePDG.Instance().GetParticle(321).Mass()
 
         weights1 = [0.78, 0.80, 0.77]
         radii1 = [0.86, 0.95, 0.79]
@@ -117,7 +165,8 @@ if __name__ == '__main__':
         print("not implemented")
         sys.exit()
 
-    mLF = TDatabasePDG.Instance().GetParticle(lpdg).Mass()
+    normRange = [1500, 2000]
+    heavyMass = TDatabasePDG.Instance().GetParticle(411).Mass()
 
     # load yaml file with lambda parameter
     with open(config, "r") as stream:
@@ -162,7 +211,7 @@ if __name__ == '__main__':
             rebinFactor = round(kStarBW/hSEData.GetBinWidth(1))
             hSEData.Rebin(rebinFactor)
             hMEData.Rebin(rebinFactor)
-            hSEData.Scale(ComputeNormFactor(hSEData, hMEData, 1000, 1500))
+            hSEData.Scale(ComputeNormFactor(hSEData, hMEData, normRange[0], normRange[1]))
             hCFData = hSEData/hMEData
             hSEData.Write()
             hMEData.Write()
@@ -189,6 +238,67 @@ if __name__ == '__main__':
         hCFBkg = lamPar['sb'] * dCFData['sbr'] + blNorm * hCFMC * (lamPar['gen'] + lamPar['flat'])
         hCFBkg.SetName('hCFBkg')
         hCFBkg.Write()
+
+        # Compute the Gen CF
+        hCFFlat = MakeFlatHist(dCFData['sgn'], lamPar['flat'], 'hCFFlat')
+        hCFGen = (dCFData['sgn'] - lamPar['sb'] * dCFData['sbr'])/(blNorm * hCFMC) - hCFFlat
+        hCFGen.Scale(1./lamPar['gen'])
+        hCFGen.SetName('hCFGen')
+        hCFGen.Write()
+
+        # Compute the coulomb-only CF with Lednicky
+        fitRange = fitRanges[0]
+        fCoulomb = TF1("fCoulomb", WeightedCoulombLednicky, fitRange[0], fitRange[1], 8)
+        fCoulomb.FixParameter(0, radii1[0])
+        fCoulomb.FixParameter(1, radii2[0])
+        fCoulomb.FixParameter(2, weights1[0])
+        fCoulomb.FixParameter(3, 0)
+        fCoulomb.FixParameter(4, 0.)
+        fCoulomb.FixParameter(5, 0.)
+        fCoulomb.FixParameter(6, 1 if comb == 'sc' else -1)
+        fCoulomb.FixParameter(7, RedMass(heavyMass, lightMass) * 1000)
+        fCoulomb.SetLineColor(kBlue)
+
+        # Fit the CF
+        gCFGen = ApplyCenterOfGravity(hCFGen, gGravities)
+        fWeightedLL = TF1("fWeightedLL", WeightedCoulombLednicky, fitRange[0], fitRange[1], 8)
+        fWeightedLL.FixParameter(0, radii1[0])
+        fWeightedLL.FixParameter(1, radii2[0])
+        fWeightedLL.FixParameter(2, weights1[0])
+        fWeightedLL.SetParameter(3, 0.1)
+        fWeightedLL.SetParLimits(3, -1, 1)
+        fWeightedLL.FixParameter(4, 0.)
+        fWeightedLL.FixParameter(5, 0.)
+        fWeightedLL.FixParameter(6, 1 if comb == 'sc' else -1)
+        fWeightedLL.FixParameter(7, RedMass(heavyMass, lightMass) * 1000)
+
+        status = gCFGen.Fit(fWeightedLL, "SMRQ+0").Status()
+        chi2ndf = fWeightedLL.GetChisquare()/fWeightedLL.GetNDF()
+        scattLen = fWeightedLL.GetParameter(3)
+        scattLenUnc = fWeightedLL.GetParError(3)
+
+        cFit = TCanvas(f'cFit_{comb}', '', 600, 600)
+        cFit.DrawFrame(0, 0, 500, 2)
+        hCFGen.Draw('pe')
+        fWeightedLL.Draw('same')
+        fCoulomb.Draw('same')
+
+        step = 0.05
+        iVar = 0
+        tl = TLatex()
+        tl.SetNDC()
+        tl.SetTextSize(0.035)
+        tl.SetTextFont(42)
+        tl.DrawLatex(0.2, 0.85 - 0 * step, fempy.utils.format.TranslateToLatex(f'k{args.pair}_{comb}   iVar: {iVar}'))
+        tl.DrawLatex(0.2, 0.85 - 1 * step, f'a_{{0}} = {scattLen:.3f} #pm {scattLenUnc:.3f}')
+        tl.DrawLatex(0.2, 0.85 - 2 * step, f'#chi^{{2}}/ndf = {chi2ndf:.2f}')
+
+        leg = TLegend(0.5, 0.7, 0.85, 0.85)
+        leg.AddEntry(hCFGen, 'Data')
+        leg.AddEntry(fWeightedLL, 'Fit LL')
+        leg.AddEntry(fCoulomb, 'Coulomb LL')
+        leg.Draw()
+        cFit.Write()
 
     print(f'output saved in {oFileName}')
     oFile.Close()
