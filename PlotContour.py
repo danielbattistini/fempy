19a1adea5d5f42a3a80e32c13054b773c2c6e9f5
Daniel Battistini <daniel.battistini@cern.ch>
2023-06-02 16:40:57 +0200

    debug info on plots

--- PlotContour.py
+++ PlotContour.py
@@ -103,6 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('inFile')
     parser.add_argument('oFile')
     parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--shift', action='store_true', default=False)
     parser.add_argument('--central', choices=('statbs', 'totbs'))
     args = parser.parse_args()
 
@@ -134,15 +135,25 @@ if __name__ == '__main__':
     # Plot a transparent 3 standard deviation covariance ellipse
     ellip2Tot = plot_point_cov(pointsTot.T, nstd=2, color=albumen, label='95% CL')
     ellip1Tot = plot_point_cov(pointsTot.T, nstd=1, color=yoke, label='68% CL')
+    
+    ellip2TotShifted = plot_point_cov(pointsTot.T, nstd=2, color=albumen, label='95% CL')
+    ellip2TotShifted.center = (a0sinStat, a0triStat)
+    ellip1TotShifted = plot_point_cov(pointsTot.T, nstd=1, color=yoke, label='68% CL')
+    ellip1TotShifted.center = (a0sinStat, a0triStat)
+
+    absShift = (abs(a0sinTot - a0sinStat), abs(a0triTot - a0triStat))
+    ellip2TotShiftedEnlarged = plot_point_cov(pointsTot.T, nstd=2, color=albumen, enlarge=absShift, label='95% CL')
+    ellip2TotShiftedEnlarged.center = (a0sinStat, a0triStat)
+    ellip1TotShiftedEnlarged = plot_point_cov(pointsTot.T, nstd=1, color=yoke, enlarge=absShift, label='68% CL')
+    ellip1TotShiftedEnlarged.center = (a0sinStat, a0triStat)
+
     ellip2Stat = plot_point_cov(pointsStat.T, nstd=2, color=albumen, label='95% CL')
     ellip1Stat = plot_point_cov(pointsStat.T, nstd=1, color=yoke, label='68% CL')
 
     a0sinUncSyst = (a0sinUncTot**2 - a0sinUncStat**2)**0.5
     a0triUncSyst = (a0triUncTot**2 - a0triUncStat**2)**0.5
 
-    print(f'a0(3/2) = {a0sinTot:.2f} +/- {a0sinUncStat:.2f} (stat) +/- {a0sinUncSyst:.2f} (syst) fm')
-    print(f'a0(1/2) = {a0triTot:.2f} +/- {a0triUncStat:.2f} (stat) +/- {a0triUncSyst:.2f} (syst) fm')
-
+    
     if args.debug:
         pointsStat = np.array(list(RDataFrame(tResultsStat).AsNumpy(['a0sin', 'a0tri']).values()))
 
@@ -162,12 +173,14 @@ if __name__ == '__main__':
     tlALICE = TLatex()
     tlALICE.SetTextFont(42)
     tlALICE.SetTextSize(0.06)
-    tlALICE.DrawLatexNDC(0.36, 0.89, 'ALICE')
+    if not args.debug:
+        tlALICE.DrawLatexNDC(0.36, 0.89, 'ALICE')
 
     tl = TLatex()
     tl.SetTextFont(42)
     tl.SetTextSize(0.039)
-    tl.DrawLatexNDC(0.36, 0.89-0.05, 'pp #sqrt{s} = 13 TeV, High-mult. (0-0.17%)')
+    if not args.debug:
+        tl.DrawLatexNDC(0.36, 0.89-0.05, 'pp #sqrt{s} = 13 TeV, High-mult. (0-0.17%)')
 
     gScattParTot = TGraphErrors(1)
     gScattParTot.SetPoint(0, a0sinTot, a0triTot)
@@ -177,17 +190,94 @@ if __name__ == '__main__':
     gScattParTot.SetMarkerColor(1)
     gScattParTot.SetLineWidth(2)
 
+    gScattParStat = TGraphErrors(1)
+    gScattParStat.SetPoint(0, a0sinStat, a0triStat)
+    gScattParStat.SetPointError(0, a0sinUncTot, a0triUncTot)
+    gScattParStat.SetMarkerStyle(20)
+    gScattParStat.SetMarkerSize(1)
+    gScattParStat.SetMarkerColor(1)
+    gScattParStat.SetLineWidth(2)
+
+    gScattParStatCentrTotPlusShift = TGraphErrors(1)
+    gScattParStatCentrTotPlusShift.SetPoint(0, a0sinStat, a0triStat)
+    a0sinUncTotPlusShift = np.sqrt(a0sinUncTot**2 + absShift[0]**2)
+    a0triUncTotPlusShift = np.sqrt(a0triUncTot**2 + absShift[1]**2)
+    a0sinUncSystPlusShift = np.sqrt(a0sinUncTotPlusShift**2 - a0sinUncStat**2)
+    a0triUncSystPlusShift = np.sqrt(a0triUncTotPlusShift**2 - a0triUncStat**2)
+    gScattParStatCentrTotPlusShift.SetPointError(0, a0sinUncTotPlusShift, a0triUncTotPlusShift)
+
+
+    print('\ncentr total')
+    print(f'a0(3/2) = {a0sinTot:.2f} +/- {a0sinUncStat:.2f} (stat) +/- {a0sinUncSyst:.2f} (syst) fm')
+    print(f'a0(1/2) = {a0triTot:.2f} +/- {a0triUncStat:.2f} (stat) +/- {a0triUncSyst:.2f} (syst) fm')
+
+    print('\ncentr stat')
+    print(f'a0(3/2) = {a0sinStat:.2f} +/- {a0sinUncStat:.2f} (stat) +/- {a0sinUncSyst:.2f} (syst) fm')
+    print(f'a0(1/2) = {a0triStat:.2f} +/- {a0triUncStat:.2f} (stat) +/- {a0triUncSyst:.2f} (syst) fm')
+
+    print('\ncentr stat, shift and enlarge')
+    print(f'a0(3/2) = {a0sinStat:.2f} +/- {a0sinUncStat:.2f} (stat) +/- {a0sinUncSystPlusShift:.2f} (syst) fm')
+    print(f'a0(1/2) = {a0triStat:.2f} +/- {a0triUncStat:.2f} (stat) +/- {a0triUncSystPlusShift:.2f} (syst) fm')
+
+
     gCountour2Tot = EllipseToGraph(ellip2Tot)
+    gCountour1Tot = EllipseToGraph(ellip1Tot)
+    gCountour2Stat = EllipseToGraph(ellip2Stat)
+    gCountour1Stat = EllipseToGraph(ellip1Stat)
+    
+    gCountour1TotShifted = EllipseToGraph(ellip1TotShifted)
+    gCountour2TotShifted = EllipseToGraph(ellip2TotShifted)
+    
+    gCountour1TotShiftedEnlarged = EllipseToGraph(ellip1TotShiftedEnlarged)
+    gCountour2TotShiftedEnlarged = EllipseToGraph(ellip2TotShiftedEnlarged)
+
+    if args.central == 'statbs':
+        if args.shift:
+            gContourToFill1 = gCountour1TotShiftedEnlarged
+            gContourToFill2 = gCountour2TotShiftedEnlarged
+        else:
+            gContourToFill1 = gCountour1Stat
+            gContourToFill2 = gCountour2Stat
+
+        gContourToDash1 = gCountour1Tot
+        gContourToDash2 = gCountour2Tot
+            
+        dLeg = {
+            'fill1': '68% CL stat bs',
+            'fill2': '95% CL stat bs',
+            'dash1': '68% CL tot bs',
+            'dash2': '95% CL tot bs',
+        }
+    elif args.central == 'totbs':
+        gContourToFill1 = gCountour1Tot
+        gContourToFill2 = gCountour2Tot
+    
+        gContourToDash1 = gCountour1Stat
+        gContourToDash2 = gCountour2Stat
+        dLeg = {
+            'fill1': '68% CL tot bs',
+            'fill2': '95% CL tot bs',
+            'dash1': '68% CL stat bs',
+            'dash2': '95% CL stat bs',
+        }
+
     ciAlbumen = TColor.GetFreeColorIndex()
     albumenRGB = rgb(albumen)
     albumenRoot = TColor(ciAlbumen, albumenRGB[0], albumenRGB[1], albumenRGB[2], '')
-    gCountour2Tot.SetFillColor(ciAlbumen)
+    gContourToFill2.SetFillColor(ciAlbumen)
 
-    gCountour1Tot = EllipseToGraph(ellip1Tot)
     yokeRGB = rgb(yoke)
     ciYoke = TColor.GetFreeColorIndex()
     yokeRoot = TColor(ciYoke, yokeRGB[0], yokeRGB[1], yokeRGB[2], '')
-    gCountour1Tot.SetFillColorAlpha(ciYoke, 1)
+    gContourToFill1.SetFillColorAlpha(ciYoke, 1)
+
+    gContourToDash1.SetLineStyle(9)
+    gContourToDash1.SetLineWidth(2)
+    gContourToDash1.SetLineColor(4)
+
+    gContourToDash2.SetLineStyle(9)
+    gContourToDash2.SetLineWidth(2)
+    gContourToDash2.SetLineColor(4)
 
     lh = TLine(0, ylim[0], 0, ylim[1])
     lh.SetLineStyle(9)
@@ -197,21 +287,92 @@ if __name__ == '__main__':
     lv.SetLineStyle(9)
     lv.SetLineColor(kGray+2)
     
+    leg = TLegend(0.56, 0.57, 0.95, 0.78)
     if args.debug:
-        leg = TLegend(0.34, 0.57, 0.95, 0.78)
-    else:
-        leg = TLegend(0.65, 0.57, 0.95, 0.78)
-    leg.AddEntry(gScattParTot, 'Data', 'lpe')
-    leg.AddEntry(gCountour1Tot, '68% CL tot bs', 'f')
-    leg.AddEntry(gCountour2Tot, '95% CL tot bs', 'f')
-    leg.SetBorderSize(0)
-    leg.Draw()
+        leg = TLegend(0.34, 0.55, 0.95, 0.95)
+        leg.AddEntry(gScattParTot, 'Data tot bs', 'lpe')
+        if args.central == 'statbs':
+            gScattParStat.SetLineColor(1)
+            gScattParStat.SetMarkerColor(1)
+            gScattParTot.SetLineColor(4)
+            gScattParTot.SetMarkerColor(4)
+        else:
+            gScattParStat.SetLineColor(4)
+            gScattParStat.SetMarkerColor(4)
+            gScattParTot.SetLineColor(1)
+            gScattParTot.SetMarkerColor(1)
 
-    gCountour2Tot.Draw('same fce3')
-    gCountour1Tot.Draw('same fce3')
+        leg.AddEntry(gScattParStat, 'Data stat bs', 'lpe')
+        leg.AddEntry(gScattParStatCentrTotPlusShift, 'Data stat bs, tot #oplus shift', 'lpe')
+
+        leg.AddEntry(gContourToFill1, dLeg['fill1'], 'f')
+        leg.AddEntry(gContourToFill2, dLeg['fill2'], 'f')
+        leg.AddEntry(gContourToDash1, dLeg['dash1'], 'l')
+        leg.AddEntry(gContourToDash2, dLeg['dash2'], 'l')
+        gContourToFill2.Draw('same fce3')
+        gContourToFill1.Draw('same fce3')
+        gContourToDash2.Draw('same l')
+        gContourToDash1.Draw('same l')
+
+        gCountour2TotShifted.SetLineStyle(8)
+        gCountour2TotShifted.SetLineColor(2)
+
+        gCountour1TotShifted.SetLineStyle(8)
+        gCountour1TotShifted.SetLineColor(2)
+        gCountour1TotShifted.Draw('same l')
+        gCountour2TotShifted.Draw('same l')
+
+        leg.AddEntry(gCountour1TotShifted, '68% CL tot bs shifted', 'l')
+        leg.AddEntry(gCountour2TotShifted, '95% CL tot bs shifted', 'l')
+
+        gCountour2TotShiftedEnlarged.SetLineStyle(7)
+        gCountour2TotShiftedEnlarged.SetLineColor(3)
+
+        gCountour1TotShiftedEnlarged.SetLineStyle(7)
+        gCountour1TotShiftedEnlarged.SetLineColor(3)
+        gCountour1TotShiftedEnlarged.Draw('same l')
+        gCountour2TotShiftedEnlarged.Draw('same l')
+
+        leg.AddEntry(gCountour1TotShiftedEnlarged, '68% CL tot bs shifted enlarged', 'l')
+        leg.AddEntry(gCountour2TotShiftedEnlarged, '95% CL tot bs shifted enlarged', 'l')
+        
+        gScattParStatCentrTotPlusShift.SetMarkerStyle(25)
+        gScattParStatCentrTotPlusShift.SetMarkerSize(2)
+        gScattParStatCentrTotPlusShift.SetMarkerColor(617)
+        gScattParStatCentrTotPlusShift.SetLineColor(617)
+        gScattParStatCentrTotPlusShift.SetLineWidth(2)
+        gScattParStatCentrTotPlusShift.Draw('same lpe')
+
+    else:
+        leg.AddEntry(gContourToFill1, '68% CL', 'f')
+        leg.AddEntry(gContourToFill2, '95% CL', 'f')
+        
+        gContourToFill2.Draw('same fce3')
+        gContourToFill1.Draw('same fce3')
+            
+        leg.AddEntry(gScattParStatCentrTotPlusShift, 'Data', 'lpe')
+
+        gCountour1TotShiftedEnlarged.SetLineColorAlpha(0, 0)
+        gCountour2TotShiftedEnlarged.SetLineColorAlpha(0, 0)
+        gCountour1TotShiftedEnlarged.Draw('same l')
+        gCountour2TotShiftedEnlarged.Draw('same l')
+
+        gScattParStatCentrTotPlusShift.SetMarkerStyle(20)
+        gScattParStatCentrTotPlusShift.SetMarkerSize(1)
+        gScattParStatCentrTotPlusShift.SetMarkerColor(1)
+        gScattParStatCentrTotPlusShift.SetLineColor(1)
+        gScattParStatCentrTotPlusShift.SetLineWidth(2)
+        gScattParStatCentrTotPlusShift.Draw('same lpe')
+
+    leg.SetBorderSize(0)
+
     lv.Draw()
     lh.Draw()
-    gScattParTot.Draw('same pe')
+    leg.Draw()
+
+    if args.debug:
+        gScattParStat.Draw('same pe')
+
     for ext in ['png', 'pdf', 'eps', 'root']:
         if args.debug:
             name = f'{path.splitext(args.oFile)[0]}_root_debug.{ext}'
