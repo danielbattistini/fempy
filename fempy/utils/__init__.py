12ce5e9ec68643e52c83b077783df4fa8c5d5872
danielbattistini <daniel.battistini@cern.ch>
2023-06-07 13:18:09 +0200

    Add method to merge root files in chunks

--- fempy/utils/__init__.py
+++ fempy/utils/__init__.py
@@ -3,6 +3,28 @@ from .analysis import Pair
 from .io import *
 from .format import *
 
+def Chunkyfy(array, nElemPerChunk=200):
+    if len(array) == 0:
+        return [[]]
+    nChunks = (len(array) -1) // nElemPerChunk +1
+    return [array[iChunk*nElemPerChunk:nElemPerChunk * (iChunk +1)] for iChunk in range(nChunks)]
+
+
+def MergeInChunks(mergeList, oFile, nElemPerChunk=200):
+    # perform the mergin in 2 steps. Temporary merge files are not targets of the task
+    tmpFiles = []
+    for iMerge, mList in enumerate(Chunkyfy(mergeList, nElemPerChunk)):
+        print(mList)
+        tmpFiles.append(os.path.splitext(oFile)[0] + f'_{iMerge}.root ')
+        command = 'hadd -f ' + tmpFiles[-1] + ' '.join(mList)
+        print(command)
+        os.system(command)
+
+    # Final merging    
+    command = 'hadd -f ' + oFile + ' ' + ' '.join(tmpFiles)
+    os.system(command)
+
+
 def DivideCanvas(canvas, n):
     if n <= 1:
         return canvas
