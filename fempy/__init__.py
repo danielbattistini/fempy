4e8fd08756b9ffea50daf34517da726e212bec95
Daniel Battistini <daniel.battistini@cern.ch>
2023-08-10 15:33:59 +0200

    Add logger

--- fempy/__init__.py
+++ fempy/__init__.py
@@ -5,8 +5,45 @@ from ROOT import gROOT, gStyle
 from .correlation_function import CorrelationFunction
 from .utils import *
 
+print("Welcome to fempy!")
+
 gROOT.SetBatch(True)
 
+import logging
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[90;20m"
+    blue = "\x1b[34;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    purple = "\x1b[35;1m"
+    reset = "\x1b[0m"
+    # style = "%(module)s::%(funcName)s %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    style = "%(module)s::%(funcName)s %(levelname)s - %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + style + reset,
+        logging.INFO: blue + style + reset,
+        logging.WARNING: yellow + style + reset,
+        logging.ERROR: red + style + reset,
+        logging.CRITICAL: purple + style + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+# create logger with 'spam_application'
+logger = logging.getLogger("fempy")
+logger.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
 
 # set style
 def set_style(style):
