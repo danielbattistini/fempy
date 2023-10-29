226871ab3a88a9110475b0d7fd8acb30ec847e42
Daniel Battistini <daniel.battistini@cern.ch>
2023-10-29 20:34:20 +0100

    Update logger

--- fempy/__init__.py
+++ fempy/__init__.py
@@ -1,32 +1,36 @@
 import sys
+import logging
+
+from .utils import *
+from .correlation_function import CorrelationFunction
 
 from ROOT import gROOT, gStyle
 
-from .correlation_function import CorrelationFunction
-from .utils import *
 
-print("Welcome to fempy!")
-
-gROOT.SetBatch(True)
-
-import logging
-
+# Create logging formatter for beautiful log messages
 class CustomFormatter(logging.Formatter):
+    '''
+    DEBUG: for very detailed output
+    INFO: for tracking the progress of the program
+    WARNING: for something unexpected that doesn't cause problems
+    ERROR: for something unexpected that can cause problems
+    CRITICAL: for a failure that cause the program to terminate
+    '''
     grey = "\x1b[90;20m"
     blue = "\x1b[34;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
     purple = "\x1b[35;1m"
     reset = "\x1b[0m"
-    # style = "%(module)s::%(funcName)s %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    style = "%(module)s::%(funcName)s %(levelname)s - %(message)s"
+    style = "%(module)s::%(funcName)s %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    criticalStyle = "%(module)s::%(funcName)s %(levelname)s - %(message)s ---> EXIT! (%(filename)s:%(lineno)d)"
 
     FORMATS = {
         logging.DEBUG: grey + style + reset,
         logging.INFO: blue + style + reset,
         logging.WARNING: yellow + style + reset,
         logging.ERROR: red + style + reset,
-        logging.CRITICAL: purple + style + reset
+        logging.CRITICAL: purple + criticalStyle + reset
     }
 
     def format(self, record):
@@ -34,14 +38,23 @@ class CustomFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
+# Create handler to exit the program for CRITICAL errors
+class ExitOnExceptionHandler(logging.StreamHandler):
+    '''Exit on CRITICAL'''
 
-# create logger with 'spam_application'
+    def emit(self, record):
+        super().emit(record)
+        if record.levelno is logging.CRITICAL:
+            raise SystemExit(-1)
+
+
+print("Welcome to fempy!")
+gROOT.SetBatch(True)
+
+# create logger
 logger = logging.getLogger("fempy")
-logger.setLevel(logging.DEBUG)
-
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+ch = ExitOnExceptionHandler()
 ch.setFormatter(CustomFormatter())
 logger.addHandler(ch)
 
