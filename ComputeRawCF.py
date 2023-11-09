4fbc18d28c615bd33b6a4bbc171518795e3b226f
Marcellocosti <marcellodicostanzo00@gmail.com>
2023-11-09 12:42:30 +0100

    reimplementation of file folders names

--- ComputeRawCF.py
+++ ComputeRawCF.py
@@ -32,14 +32,11 @@ with open(args.cfg, "r") as stream:
 
 regions = ['sgn']
 combs = {
-    cfg['pairs'][0]: 'Particle0_Particle2',
-    cfg['pairs'][1]: 'Particle0_Particle3',
-    cfg['pairs'][2]: 'Particle1_Particle2',
-    cfg['pairs'][3]: 'Particle1_Particle3'
+    'p02': 'Particle0_Particle2',
+    'p03': 'Particle0_Particle3',
+    'p12': 'Particle1_Particle2',
+    'p13': 'Particle1_Particle3'
 }
-
-firstpair = cfg['pairs'][0] + cfg['pairs'][3]
-secondpair = cfg['pairs'][1] + cfg['pairs'][2]
 
 # Load input file with same- and mixed-event distributions
 inFile = TFile(cfg['infile'])
@@ -67,40 +64,39 @@ for comb, fdcomb in combs.items():
     hME[comb] = {}
     hMErew[comb] = {}
     for region in regions:
-        hSE[comb][region] = Load(inFile, f'HMResults1001/HMResults1001;1/{fdcomb}/SEDist_{fdcomb}')
-        hME[comb][region] = Load(inFile, f'HMResults1001/HMResults1001;1/{fdcomb}/MEDist_{fdcomb}')
-        binwidth = cfg['multbinwidth']
-        hSEmultk = Load(inFile, f'HMResults1001/HMResults1001;1/{fdcomb}/SEMultDist_{fdcomb}')
-        hMEmultk = Load(inFile, f'HMResults1001/HMResults1001;1/{fdcomb}/MEMultDist_{fdcomb}')
+        pcsuffix = cfg['pcsuffix']
+        hSE[comb][region] = Load(inFile, f'HMResults{pcsuffix}/HMResults{pcsuffix}/{fdcomb}/SEDist_{fdcomb}')
+        hME[comb][region] = Load(inFile, f'HMResults{pcsuffix}/HMResults{pcsuffix}/{fdcomb}/MEDist_{fdcomb}')
+        hSEmultk = Load(inFile, f'HMResults{pcsuffix}/HMResults{pcsuffix}/{fdcomb}/SEMultDist_{fdcomb}')
+        hMEmultk = Load(inFile, f'HMResults{pcsuffix}/HMResults{pcsuffix}/{fdcomb}/MEMultDist_{fdcomb}')
         nbins = hMEmultk.ProjectionX().GetNbinsX()
-        hMEreweightk = TH1F("MErewdistr", "MErewdistr", nbins, hMEmultk.ProjectionX().GetBinLowEdge(1), 
-                            hMEmultk.ProjectionX().GetBinLowEdge(nbins) + hMEmultk.ProjectionX().GetBinWidth(nbins))
-        for binidx in range(0, hMEmultk.ProjectionY().GetNbinsX(), binwidth):
-            hSEbinmult = hSEmultk.ProjectionX(f'{comb}SEdistr', binidx, binidx + binwidth)
-            hMEbinmult = hMEmultk.ProjectionX(f'{comb}MEdistr', binidx, binidx + binwidth)
+        hMEreweightk = TH1F("MErewdistr", "MErewdistr", nbins, hMEmultk.GetXaxis().GetXmin(), hMEmultk.GetXaxis().GetXmax())
+        for iBin in range(hMEmultk.ProjectionY().GetNbinsX()):
+            hSEbinmult = hSEmultk.ProjectionX(f'{comb}SEdistr', iBin, iBin+1)
+            hMEbinmult = hMEmultk.ProjectionX(f'{comb}MEdistr', iBin, iBin+1)
             hMEreweightk.Add(hMEbinmult, hSEbinmult.Integral()/hMEbinmult.Integral())
         hMErew[comb][region] = hMEreweightk
    
 # Sum pair and antipair
 for comb in combs:
-    hSE[firstpair] = {}
-    hSE[secondpair] = {}
-    hME[firstpair] = {}
-    hME[secondpair] = {}
-    hMErew[firstpair] = {}
-    hMErew[secondpair] = {}
+    hSE['p02_13'] = {}
+    hSE['p03_12'] = {}
+    hME['p02_13'] = {}
+    hME['p03_12'] = {}
+    hMErew['p02_13'] = {}
+    hMErew['p03_12'] = {}
 
     for region in regions:
-        hSE[firstpair][region] = hSE[cfg['pairs'][0]][region] + hSE[cfg['pairs'][3]][region]
-        hME[firstpair][region] = hME[cfg['pairs'][0]][region] + hME[cfg['pairs'][3]][region]
-        hMErew[firstpair][region] = hMErew[cfg['pairs'][0]][region] + hMErew[cfg['pairs'][3]][region]
+        hSE['p02_13'][region] = hSE['p02'][region] + hSE['p13'][region]
+        hME['p02_13'][region] = hME['p02'][region] + hME['p13'][region]
+        hMErew['p02_13'][region] = hMErew['p02'][region] + hMErew['p13'][region]
         
-        hSE[secondpair][region] = hSE[cfg['pairs'][1]][region] + hSE[cfg['pairs'][2]][region]
-        hME[secondpair][region] = hME[cfg['pairs'][1]][region] + hME[cfg['pairs'][2]][region]
-        hMErew[secondpair][region] = hMErew[cfg['pairs'][1]][region] + hMErew[cfg['pairs'][2]][region]
+        hSE['p03_12'][region] = hSE['p03'][region] + hSE['p12'][region]
+        hME['p03_12'][region] = hME['p03'][region] + hME['p12'][region]
+        hMErew['p03_12'][region] = hMErew['p03'][region] + hMErew['p12'][region]
 
 # Compute the CF and write to file
-for comb in list(combs.keys()) + [firstpair, secondpair]:
+for comb in list(combs.keys()) + ['p02_13', 'p03_12']:
     for region in regions:
         rebin = round(float(cfg['binwidth']) / (hSE[comb][region].GetBinWidth(1) * 1000))
         print(rebin)
@@ -109,28 +105,35 @@ for comb in list(combs.keys()) + [firstpair, secondpair]:
         hMErew[comb][region].Rebin(rebin)
 
         if cfg['norm'] is None:  # if not specified, normalize to the yields
-            norm = hMErew[comb][region].GetEntries() / hSE[comb][region].GetEntries()
+            norm = hME[comb][region].GetEntries() / hSE[comb][region].GetEntries()
+            normrew = hMErew[comb][region].GetEntries() / hSE[comb][region].GetEntries()
         else:
             log.critical('Normalization method not implemented')
 
         hSE[comb][region].Sumw2()
-        hCF = norm * hSE[comb][region] / hMErew[comb][region]
+        
+        hCF = norm * hSE[comb][region] / hME[comb][region]
+        hCFrew = normrew * hSE[comb][region] / hMErew[comb][region]
 
         oFile.mkdir(f'{comb}/{region}')
         oFile.cd(f'{comb}/{region}')
 
-        hSE[comb][region].SetName('hCF')
+        hSE[comb][region].SetName('hSE')
         hSE[comb][region].SetTitle(';#it{k}* (GeV/#it{c});Counts')
 
-        hME[comb][region].SetName('hCF')
+        hME[comb][region].SetName('hME')
         hME[comb][region].SetTitle(';#it{k}* (GeV/#it{c});Counts')
         
-        hMErew[comb][region].SetName('hCF')
+        hMErew[comb][region].SetName('hMErew')
         hMErew[comb][region].SetTitle(';#it{k}* (GeV/#it{c});Counts')
 
         hCF.SetName('hCF')
         hCF.SetTitle(';#it{k}* (GeV/#it{c});#it{C}(#it{k}*)')
         hCF.Write()
+        
+        hCFrew.SetName('hCFrew')
+        hCFrew.SetTitle(';#it{k}* (GeV/#it{c});#it{C}(#it{k}*)')
+        hCFrew.Write()
 
 oFile.Close()
 print(f'output saved in {oFileName}')
