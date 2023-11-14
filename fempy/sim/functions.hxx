f499acf5f7c5492916652804963e1d5dc22ae29c
danielbattistini <daniel.battistini@cern.ch>
2023-11-14 18:35:04 +0100

    Move the functions related to AliPythia in a separate file

--- fempy/sim/functions.hxx
+++ fempy/sim/functions.hxx
@@ -1,5 +1,5 @@
-#ifndef SIM_FUNCTIONS_HXX_
-#define SIM_FUNCTIONS_HXX_
+#ifndef FEMPY_SIM_FUNCTIONS_HXX_
+#define FEMPY_SIM_FUNCTIONS_HXX_
 
 #include <map>
 #include <utility>
@@ -10,7 +10,6 @@
 #include <TClonesArray.h>
 #include <TParticle.h>
 
-#include "AliPythia8.h"
 #include "Math/GenVector/Boost.h"
 #include "Math/Vector4D.h"
 
@@ -801,78 +800,8 @@ bool ThereIsD(TClonesArray *particles) {
 }
 
 // #############################################################################
-// Pythia ######################################################################
+// Misc ######################################################################
 // #############################################################################
-
-// Set the pythia tune
-void SetTune(AliPythia8 *pythia, tunes tune) {
-    if (tune == kMonash) {
-        pythia.ReadString(Form("Tune:pp = 14"));
-    } else if (tune == kCRMode0) {
-        pythia.ReadString(Form("Tune:pp = 14"));
-        pythia.ReadString("ColourReconnection:mode = 1");
-        pythia.ReadString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.ReadString("ColourReconnection:m0 = 2.9");
-        pythia.ReadString("ColourReconnection:allowJunctions = on");
-        pythia.ReadString("ColourReconnection:junctionCorrection = 1.43");
-        pythia.ReadString("ColourReconnection:timeDilationMode = 0");
-        pythia.ReadString("StringPT:sigma = 0.335");
-        pythia.ReadString("StringZ:aLund = 0.36");
-        pythia.ReadString("StringZ:bLund = 0.56");
-        pythia.ReadString("StringFlav:probQQtoQ = 0.078");
-        pythia.ReadString("StringFlav:ProbStoUD = 0.2");
-        pythia.ReadString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.ReadString("MultiPartonInteractions:pT0Ref = 2.12");
-        pythia.ReadString("BeamRemnants:remnantMode = 1");
-        pythia.ReadString("BeamRemnants:saturation = 5");
-    } else if (tune == kCRMode2) {
-        pythia.ReadString(Form("Tune:pp = 14"));
-        pythia.ReadString("ColourReconnection:mode = 1");
-        pythia.ReadString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.ReadString("ColourReconnection:m0 = 0.3");
-        pythia.ReadString("ColourReconnection:allowJunctions = on");
-        pythia.ReadString("ColourReconnection:junctionCorrection = 1.20");
-        pythia.ReadString("ColourReconnection:timeDilationMode = 2");
-        pythia.ReadString("ColourReconnection:timeDilationPar = 0.18");
-        pythia.ReadString("StringPT:sigma = 0.335");
-        pythia.ReadString("StringZ:aLund = 0.36");
-        pythia.ReadString("StringZ:bLund = 0.56");
-        pythia.ReadString("StringFlav:probQQtoQ = 0.078");
-        pythia.ReadString("StringFlav:ProbStoUD = 0.2");
-        pythia.ReadString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.ReadString("MultiPartonInteractions:pT0Ref = 2.15");
-        pythia.ReadString("BeamRemnants:remnantMode = 1");
-        pythia.ReadString("BeamRemnants:saturation = 5");
-    } else if (tune == kCRMode3) {
-        pythia.ReadString(Form("Tune:pp = 14"));
-        pythia.ReadString("ColourReconnection:mode = 1");
-        pythia.ReadString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.ReadString("ColourReconnection:m0 = 0.3");
-        pythia.ReadString("ColourReconnection:allowJunctions = on");
-        pythia.ReadString("ColourReconnection:junctionCorrection = 1.15");
-        pythia.ReadString("ColourReconnection:timeDilationMode = 3");
-        pythia.ReadString("ColourReconnection:timeDilationPar = 0.073");
-        pythia.ReadString("StringPT:sigma = 0.335");
-        pythia.ReadString("StringZ:aLund = 0.36");
-        pythia.ReadString("StringZ:bLund = 0.56");
-        pythia.ReadString("StringFlav:probQQtoQ = 0.078");
-        pythia.ReadString("StringFlav:ProbStoUD = 0.2");
-        pythia.ReadString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.ReadString("MultiPartonInteractions:pT0Ref = 2.05");
-        pythia.ReadString("BeamRemnants:remnantMode = 1");
-        pythia.ReadString("BeamRemnants:saturation = 5");
-    }
-}
-
-// Set the pythia process
-void SetProcess(AliPythia8 *pythia, processes process) {
-    if (process == kSoftQCD) {
-        pythia.ReadString("SoftQCD:nonDiffractive = on");
-    } else if (process == kHardQCD) {
-        pythia.ReadString("HardQCD:hardccbar = on");
-        pythia.ReadString("HardQCD:hardbbbar = on");
-    }
-}
 
 inline double ProductionRadius(TParticle *p) {
     return std::sqrt(p->Vx() * p->Vx() + p->Vy() * p->Vy() + p->Vz() * p->Vz());
@@ -895,4 +824,4 @@ std::vector<ROOT::Math::PxPyPzMVector> GetLorentzVectors(TClonesArray *particles
     return part;
 }
 
-#endif  // SIM_FUNCTIONS_HXX_
+#endif  // FEMPY_SIM_FUNCTIONS_HXX_
