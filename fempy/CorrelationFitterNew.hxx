9426b5488bac170c439a8d3e5a789066a266c8fe
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-02-07 17:02:34 +0100

    before baseline implementation

--- fempy/CorrelationFitterNew.hxx
+++ fempy/CorrelationFitterNew.hxx
@@ -43,7 +43,7 @@ class CorrelationFitterNew {
     It is your responsibility to give the correct number of parameters. All fit parameters must be initialized.
     */
 
-    void Add(std::string name, std::vector<std::tuple<std::string, double, double, double>> pars, std::string addmode="") {
+    void Add(TString name, std::vector<std::tuple<std::string, double, double, double>> pars, std::string addmode="") {
         //cout << "Number of parameters: ";
         //cout << pars.size() << endl;
 
@@ -73,8 +73,17 @@ class CorrelationFitterNew {
         } else if (name == "ComplexLednicky_Singlet_doublegaussian_lambda") { // Complex Lednicky
             this->fFitFunc.push_back(ComplexLednicky_Singlet_doublegaussian_lambda);
             this->fFitFuncComps.push_back(name);
+        } else if (name == "landau") { // Complex Lednicky
+            this->fFitFunc.push_back(Landau);
+            this->fFitFuncComps.push_back(name);
+        } else if (name == "sillkstar") { // // breit wigner kstar dependent
+            this->fFitFunc.push_back(BreitWignerKStar);
+            this->fFitFuncComps.push_back(name);
         } else if (name == "spline3") { // Spline3
             this->fFitFunc.push_back(Spline3);
+            this->fFitFuncComps.push_back(name);
+        } else if (name == "spline3range") { // breit wigner kstar dependent
+            this->fFitFunc.push_back(Spline3Range);
             this->fFitFuncComps.push_back(name);
         } else {
             //printf("Error: function '%s' is not implemented. Exit!", name.data());
@@ -89,24 +98,96 @@ class CorrelationFitterNew {
         }
     }
 
+    void AddSplineHisto(TString name, TH1* splinedhisto, std::vector<std::tuple<std::string, double, double, double>> pars, std::string addmode="") {
+
+        this->fFitFuncComps.push_back(name);
+        this->fAddModes.push_back(addmode);
+        
+        this->fNPars.push_back(pars.size());
+        // Save fit settings
+        for (const auto &par : pars) {
+            this->fFitPars.insert({this->fFitPars.size(), par});
+        }
+        
+        TH1D *splineHisto = static_cast<TH1D*>(splinedhisto);
+        TSpline3* sp3 = new TSpline3(splinedhisto);
+        this->fFitSplines.push_back(sp3);
+        cout << "Added spline" << endl;
+        cout << endl;
+    }
+
+    void DrawSpline(TVirtualPad *pad, TH1* hist) {
+        pad->cd();
+        //double yMaxDraw = 1.3 * fDataHist->GetMaximum();
+
+        TH1D* histo = static_cast<TH1D*>(hist);
+        double yMaxDraw = histo->GetBinContent(histo->GetMaximumBin())*1.2;
+        fDataHist->GetYaxis()->SetRangeUser(0, yMaxDraw);
+        gPad->DrawFrame(fFitRangeMin, 0, fFitRangeMax, yMaxDraw,
+                        Form("%s;%s;%s", this->fDataHist->GetTitle(), this->fDataHist->GetXaxis()->GetTitle(),
+                             this->fDataHist->GetYaxis()->GetTitle()));
+
+        TSpline3* sp3 = new TSpline3(histo);
+        sp3->SetNpx(300);
+        sp3->SetLineColor(kRed);
+        sp3->Draw("same");
+
+        //this->fFitSplines.back()->SetNpx(300);
+        //this->fFitSplines.back()->SetLineColor(kRed);
+        //this->fFitSplines.back()->Draw("same");
+
+        histo->SetMarkerSize(0.5);
+        histo->SetMarkerStyle(20);
+        histo->SetMarkerColor(kBlack);
+        histo->SetLineColor(kBlack);
+        histo->SetLineWidth(2);
+        histo->Draw("same pe");
+        pad->Update();
+
+    }
+
     // Perform the fit
     int Fit() {
-        
+        cout << "Fitting" << endl;
+        int totTerms = this->fFitFunc.size() + this->fFitSplines.size();
+        cout << "Total terms: " << this->fFitFunc.size() << " + " << this->fFitSplines.size() << endl;
         // Build the fit function
         this->fFit = new TF1(
             "fFit",
             [&, this](double *x, double *pars) {
                 double result = 0;
+                int nTerms = this->fFitFunc.size() + this->fFitSplines.size();
                 int nPar = 0;
-                for (int iTerm = 0; iTerm < this->fFitFunc.size(); iTerm++) {
-                    auto func = this->fFitFunc[iTerm];
-                    if(fAddModes[iTerm] == "mult") {
-                        double partResult = result; 
-                        result = pars[nPar]*func(x, &pars[nPar+1])*partResult;
+                int nSplineComp = 0;
+                int nFuncComp = 0;
+                //cout << "nSplineComp: " << nSplineComp << "  " << "nFuncComp: " << nFuncComp << endl;
+                //cout << "nTerms: " << nTerms << endl;
+                //cout << "nSplineComp: " << nSplineComp << "  " << "nFuncComp: " << nFuncComp << endl;
+                for (int iTerm = 0; iTerm < nTerms; iTerm++) {
+
+                    if(fFitFuncComps[iTerm].Contains("splinehisto")) {
+                        auto func = this->fFitSplines[nSplineComp];
+                        if(fAddModes[iTerm] == "mult") {
+                            double partResult = result; 
+                            result = pars[nPar]*this->fFitSplines[nSplineComp]->Eval(x[0])*partResult;
+                        }
+                        else {
+                            result += pars[nPar]*this->fFitSplines[nSplineComp]->Eval(x[0]);
+                        }
+                        nSplineComp++;
                     } else {
-                        result += pars[nPar]*func(x, &pars[nPar+1]);
+                        auto func = this->fFitFunc[nFuncComp];
+                        if(fAddModes[iTerm] == "mult") {
+                            double partResult = result; 
+                            result = pars[nPar]*func(x, &pars[nPar+1])*partResult;
+                        } else {
+                            result += pars[nPar]*func(x, &pars[nPar+1]);
+                        }
+                        nFuncComp++;
                     }
+
                     nPar += this->fNPars[iTerm+1];
+                
                 }
                 return result;
             },
@@ -115,17 +196,18 @@ class CorrelationFitterNew {
         for (size_t iPar = 0; iPar < this->fFitPars.size(); iPar++) {
             auto pars = this->fFitPars[iPar];
             this->fFit->SetParName(iPar, std::get<0>(pars).data());
-            if (std::get<2>(pars) > std::get<3>(pars)) {
-                //cout << "iPar: " << iPar << " " << std::get<1>(pars) << endl;
+            if (std::get<2>(pars) >= std::get<3>(pars)) {
+                cout << "iPar" << iPar << ": " << std::get<1>(pars) << endl;
                 this->fFit->FixParameter(iPar, std::get<1>(pars));
             } else {
                 this->fFit->SetParameter(iPar, std::get<1>(pars));
-                //cout << "iPar: " << iPar << " " << std::get<2>(pars) << " " << std::get<3>(pars) << endl;
+                cout << "iPar" << iPar << ": " << std::get<2>(pars) << " " << std::get<3>(pars) << endl;
                 this->fFit->SetParLimits(iPar, std::get<2>(pars), std::get<3>(pars));
             }
         }
-
-        //cout << "Evaluation of the fit function prefit: " << fFit->Eval(0.1) << endl;
+        
+        cout << "Evaluation of the fit function prefit: " << fFit->Eval(0.1) << endl;
+        //int status = fDataHist->Fit(this->fFit, "LSMR+0", "")->Status();
         int status = fDataHist->Fit(this->fFit, "SMR+0", "")->Status();
         cout << "Evaluation of the fit function after fit: " << fFit->Eval(0.1) << endl;
         
@@ -141,53 +223,89 @@ class CorrelationFitterNew {
     */
     void Draw(TVirtualPad *pad) {
         pad->cd();
-        fDataHist->GetYaxis()->SetRangeUser(0, 1.3 * fDataHist->GetMaximum());
-        gPad->DrawFrame(fFitRangeMin, 0, fFitRangeMax, 1.3 * fDataHist->GetMaximum(),
+        //double yMaxDraw = 1.3 * fDataHist->GetMaximum();
+        double yMaxDraw = 1.15;
+
+        fDataHist->GetYaxis()->SetRangeUser(0, yMaxDraw);
+        gPad->DrawFrame(fFitRangeMin, 0, fFitRangeMax, yMaxDraw,
                         Form("%s;%s;%s", this->fDataHist->GetTitle(), this->fDataHist->GetXaxis()->GetTitle(),
                              this->fDataHist->GetYaxis()->GetTitle()));
         
         this->fFit->SetNpx(300);
         this->fFit->SetLineColor(kRed);
-        this->fFit->DrawF1(fFitRangeMin,fFitRangeMax,"same");
+        this->fFit->DrawF1(fFitRangeMin+1,fFitRangeMax,"same");
         pad->Update();
 
         cout << endl << endl;
 
-        std::vector<Color_t> colors = {kMagenta + 3, kAzure + 2, kGreen, kBlue + 2};
-        int setPars = 0;
+        std::vector<Color_t> colors = {kMagenta + 3, kAzure + 2, kGreen, kBlue + 2, kOrange};
         std::vector<TF1*> components;
-        for(int iFunc=0; iFunc<fFitFunc.size(); iFunc++) {
+        
+        int nTerms = this->fFitFunc.size() + this->fFitSplines.size();
+        int nSplineComp = 0;
+        int nFuncComp = 0;
+        int setPars = 0;
+        cout << "N pars: ";
+        for(int iNPars=0; iNPars<nTerms+1; iNPars++) {
+            cout << fNPars[iNPars] << " " << endl;
+        }
+        for(int iFunc=0; iFunc<nTerms; iFunc++) {
+            //if(iFunc==1) {setPars += fNPars[iFunc+1]; continue;}
             cout << "Evaluating component ... " << iFunc << endl;
-            components.push_back(new TF1(Form("iComp_%.0f", iFunc), fFitFunc[iFunc], fFitRangeMin, fFitRangeMax, fNPars[iFunc+1]-1));
-            for(int iPar=0; iPar<fNPars[iFunc+1]-1; iPar++) {
-                cout << "N pars: " << fNPars[iFunc+1] << endl;
-                cout << "Setting parameter: " << iPar << " at " << this->fFit->GetParameter(setPars + iPar + 1) << endl;
-                components.back()->FixParameter(iPar, this->fFit->GetParameter(setPars + iPar + 1));
+            TString funcName = this->fFitFuncComps[iFunc];
+            std::string compName = static_cast<std::string>(this->fFitFuncComps[iFunc]);
+            cout << "Name of component: " << funcName << endl;
+
+            if(funcName.Contains("splinehisto")) {
+                this->fFitFuncEval.push_back(new TF1(compName.data(),
+                    [&, this, setPars, nSplineComp](double *x, double *pars) {
+                        return this->fFit->GetParameter(setPars)*this->fFitSplines[nSplineComp]->Eval(x[0]);
+                    },
+                    fFitRangeMin, fFitRangeMax, 0));
+                
+                
+                this->fFitFuncEval.back()->SetNpx(300);
+                this->fFitFuncEval.back()->SetLineColor(colors[iFunc]);
+                this->fFitFuncEval.back()->Draw("same");
+                pad->Update();
+                cout << "Value of component after normalization at 2 MeV: " << this->fFitFuncEval.back()->Eval(2) << endl;
+                cout << endl;
+                setPars += fNPars[iFunc+1];
+                nSplineComp++;
+            } else {
+                cout << "Ciao" << endl;
+                cout << fNPars[iFunc+1] << endl;
+                components.push_back(new TF1(Form("iComp_%.0f", iFunc), fFitFunc[nFuncComp], fFitRangeMin, fFitRangeMax, fNPars[iFunc+1]-1));
+                for(int iPar=1; iPar<fNPars[iFunc+1]; iPar++) {
+                    cout << "N pars: " << fNPars[iFunc+1] << endl;
+                    cout << "Setting parameter: " << iPar << " at " << this->fFit->GetParameter(setPars + iPar) << endl;
+                    components.back()->FixParameter(iPar-1, this->fFit->GetParameter(setPars + iPar));
+                }
+                cout << "Value of raw component at 2 MeV: " << components.back()->Eval(2) << endl;
+                cout << "Multiplicative factor: " << this->fFit->GetParameter(setPars) << endl;
+                this->fFitFuncEval.push_back(new TF1(compName.data(),
+                    [&, this, nFuncComp, setPars, components](double *x, double *pars) {
+                        return this->fFit->GetParameter(setPars)*components[nFuncComp]->Eval(x[0]);},
+                    fFitRangeMin, fFitRangeMax, 0));
+                cout << "Value of component after normalization at 2 MeV: " << this->fFitFuncEval[nFuncComp]->Eval(2) << endl;
+                setPars += fNPars[iFunc+1];
+                cout << endl;
+                this->fFitFuncEval.back()->SetNpx(300);
+                this->fFitFuncEval.back()->SetLineColor(colors[iFunc]);
+                this->fFitFuncEval.back()->DrawF1(fFitRangeMin+1,fFitRangeMax,"same");
+                pad->Update();
+                nFuncComp++;
             }
-            cout << "Value of raw component at 1 MeV: " << components.back()->Eval(1) << endl;
-            cout << "Multiplicative factor: " << this->fFit->GetParameter(setPars) << endl;
-            std::string funcName = this->fFitFuncComps[iFunc];
-            this->fFitFuncEval.push_back(new TF1(funcName.data(),
-                [&, this, iFunc, setPars, components](double *x, double *pars) {
-                    return this->fFit->GetParameter(setPars)*components[iFunc]->Eval(x[0]);},
-                fFitRangeMin, fFitRangeMax, 0));
-            cout << "Value of component after normalization at 1 MeV: " << this->fFitFuncEval.back()->Eval(1) << endl;
-            setPars += fNPars[iFunc+1];
-            cout << endl;
-            this->fFitFuncEval[iFunc]->SetNpx(300);
-            this->fFitFuncEval[iFunc]->SetLineColor(colors[iFunc]);
-            this->fFitFuncEval.back()->Draw("same");
-            pad->Update();
         }
         
         printf("size %d\n", fFitFuncEval.size());
         
         for(int iEvalFunc=0; iEvalFunc<fFitFuncEval.size(); iEvalFunc++) {
-            cout << "Normalized component " << iEvalFunc << " at 1 MeV: " << this->fFitFuncEval[iEvalFunc]->Eval(1) << endl;
+            cout << "Normalized component " << iEvalFunc << " at 2 MeV: " << this->fFitFuncEval[iEvalFunc]->Eval(200) << endl;
         }
         cout << endl;
  
-        fDataHist->SetMarkerSize(1);
+        fDataHist->SetMarkerSize(0.5);
         fDataHist->SetMarkerStyle(20);
         fDataHist->SetMarkerColor(kBlack);
         fDataHist->SetLineColor(kBlack);
@@ -202,7 +320,8 @@ class CorrelationFitterNew {
     TF1 *fFit = nullptr;
 
     std::vector<double (*)(double *x, double *par)> fFitFunc;       // List of function describing each term of the CF model
-    std::vector<std::string> fFitFuncComps;                         // List of function describing each term of the CF model
+    std::vector<TString> fFitFuncComps;                             // List of function describing each term of the CF model
+    std::vector<TSpline3 *> fFitSplines;                            // List of function describing each term of the CF model
     std::vector<TF1*> fFitFuncEval;                                 // List of function describing each term of the CF model
     std::vector<int> fNPars;                                        // Keeps track of how many parameters each function has
     std::vector<std::string> fAddModes;                             // select mode of adding the contributions
@@ -215,6 +334,32 @@ class CorrelationFitterNew {
 #endif  // FEMPY_CORRELATIONFITTERNEW_HXX_
 
 
+                        //if(iFunc == 0) {
+                        //    return this->fFit->GetParameter(fNPars[1])*components[0]->Eval(x[0]); // components[1]->Eval(x[0]);
+                        //} 
+                        //if(iFunc > 2) {
+                        //    return this->fFit->GetParameter(fNPars[1] + fNPars[2])*components[2]->Eval(x[0]) + this->fFit->GetParameter(setPars)*components[iFunc]->Eval(x[0]);
+                        //} else{
+                        //    return this->fFit->GetParameter(setPars)*components[iFunc]->Eval(x[0]);
+                        //}
+                    //},
+
+//                    if(fAddModes[iTerm] == "mult") {
+//                        double partResult = result; 
+//                        if(fFitFuncComps[iTerm]=="splinehisto") {
+//                            result = pars[nPar]*sp3->Eval(x[0])*partResult;
+//                        }
+//                        else {
+//                            result = pars[nPar]*func(x, &pars[nPar+1])*partResult;
+//                        }
+//                    } else {
+//                        if(fFitFuncComps[iTerm]=="splinehisto") {
+//                            result = pars[nPar]*sp3->Eval(x[0]);
+//                        } else {
+//                            result += pars[nPar]*func(x, &pars[nPar+1]);
+//                        }
+//                    }
+
 
 //    TF1 *GetComponent(int icomp) {
 //        //return this->fFitFuncEval[icomp];
@@ -224,7 +369,7 @@ class CorrelationFitterNew {
 //    } 
 
         //for(int iFunc=0; iFunc<fFitFuncEval.size(); iFunc++) {
-        //    cout << "Drawing function" << endl;
+        //    //cout << "Drawing function" << endl;
         //    this->fFitFuncEval[iFunc]->SetNpx(300);
         //    this->fFitFuncEval[iFunc]->SetLineColor(colors[iFunc]);
         //    this->fFitFuncEval[iFunc]->Draw("same");
@@ -236,38 +381,38 @@ class CorrelationFitterNew {
         //cout << endl;
         //int setPars = 0;
         //for(int iFunc=0; iFunc<fFitFunc.size(); iFunc++) {
-        //    cout << "Evaluating component ..." << endl;
+        //    //cout << "Evaluating component ..." << endl;
         //    for(int iPar=0; iPar<fNPars[iFunc+1]; iPar++) {
-        //        cout << "iPar: " << iPar << " Value: " << this->fFit->GetParameter(setPars + iPar) << endl;
+        //        //cout << "iPar: " << iPar << " Value: " << this->fFit->GetParameter(setPars + iPar) << endl;
         //    }
         //    setPars += fNPars[iFunc+1];
-        //    cout << endl;
+        //    //cout << endl;
         //}
 
 
         //cout << "Ciao6" << endl;
         //for(int iFuncEval=0; iFuncEval<fFitFuncEval.size(); iFuncEval++) {
         //    for(int iPar=0; iPar<fFitFuncEval[iFuncEval]->GetNpar(); iPar++) {
-        //        cout << "Setted parameter: " << iPar << " at " << this->fFitFuncEval[iFuncEval]->GetParameter(iPar) << endl;
+        //        //cout << "Setted parameter: " << iPar << " at " << this->fFitFuncEval[iFuncEval]->GetParameter(iPar) << endl;
         //    }
         //}
         
 //        int setPars = 0;
 //        for(int iFunc=0; iFunc<fFitFunc.size(); iFunc++) {
-//            cout << "Evaluating component ..." << endl;
+//            //cout << "Evaluating component ..." << endl;
 //            this->fFitFuncEval.push_back(new TF1(Form("fComp_%.0f", iFunc), fFitFunc[iFunc], 
 //                                                      fFitRangeMin, fFitRangeMax, fNPars[iFunc+1]-1));
 //            //TF1 *comp = new TF1(Form("fComp_%.0f", iFunc), fFitFunc[iFunc], fFitRangeMin, fFitRangeMax, fNPars[iFunc+1]);
-//            cout << "N pars: " << fNPars[iFunc+1]-1 << endl;
+//            //cout << "N pars: " << fNPars[iFunc+1]-1 << endl;
 //            for(int iPar=0; iPar<fNPars[iFunc+1]-1; iPar++) {
-//                cout << "Setting parameter: " << iPar << " at " << this->fFit->GetParameter(setPars + iPar + 1) << endl;
+//                //cout << "Setting parameter: " << iPar << " at " << this->fFit->GetParameter(setPars + iPar + 1) << endl;
 //                this->fFitFuncEval.back()->SetParameter(iPar, this->fFit->GetParameter(setPars + iPar + 1));
 //            }
 //            //this->fFitFuncEval.push_back(new TF1(Form("fComp_%.0f", iFunc), this->fFit->GetParameter(setPars)*comp->Eval(x[0]), 
 //            //                                          fFitRangeMin, fFitRangeMax, fNPars[iFunc+1]));
 //            setPars += fNPars[iFunc+1];
 //            //cout << "Evaluation of the fit function after fit: " << fFitFuncEval.back()->Eval(0.1) << endl;
-//            cout << "Evaluated component!" << endl;
+//            //cout << "Evaluated component!" << endl;
 //            //delete comp;
 //        }
 
--- fempy/FitFunctions.cxx
+++ fempy/FitFunctions.cxx
@@ -58,6 +58,59 @@ double BreitWigner(double *x, double *par) {
     return yield * gamma / TMath::Pi() / (gamma * gamma + (kstar - mean) * (kstar - mean));
 }
 
+double BreitWignerKStar(double *x, double *par) {
+
+  // p0: normalisation
+  // p1: mass
+  // p2: width
+
+  if (x[0] < 0)
+    return 0;
+
+  double massPion = 139.57039;
+  double massLambda = 1115.683;
+
+  double kStarMJacobian = x[0]/sqrt( x[0]*x[0] + massPion*massPion ) + x[0]/sqrt( x[0]*x[0] + massLambda*massLambda );
+
+  return BreitWigner(x, par) * abs(kStarMJacobian);
+
+}
+
+//double BreitWignerKStar(double *x, double *par) {
+//
+//  // x[0]: k*
+//  // par: [0] "normalisation" constant
+//  //      [1] width
+//  //      [2] mass
+//
+//  double massPion = 139.57039;
+//  double massLambda = 1115.683;
+//
+//  if (x[0] < 0)
+//    return 0;
+//
+//  // double t = x[0];
+//
+//  double kstar = x[0];
+//  double Thresh = massPion + massLambda;
+//
+//  double MotherMass = sqrt(kstar * kstar + massPion * massPion) + sqrt(kstar * kstar + massLambda * massLambda);
+//
+//  if (MotherMass < Thresh)
+//    return 0;
+//
+//  double Width = par[1] * par[2] / sqrt(par[2] * par[2] - Thresh * Thresh);
+//  double arg0 = 2 * MotherMass / TMath::Pi();
+//  double arg1 = sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width;
+//  double arg2 = pow(MotherMass * MotherMass - par[2] * par[2], 2.);
+//  double arg3 = pow(sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width, 2.);
+//
+//  double kStarMJacobian = kstar / sqrt(kstar * kstar + massPion * massPion) + kstar / sqrt(kstar * kstar + massLambda * massLambda);
+//
+//  return (par[0] * arg0 * arg1 / (arg2 + arg3)) * abs(kStarMJacobian);
+//
+//}
+
 // Convolution of a Breit-Wigner and a gaussian
 double Voigt(double *x, double *par) {
     double kstar = x[0];
@@ -86,7 +139,7 @@ double PowEx(double *x, double *par) {
 }
 
 double Spline3(double *x, double *par){
-    int numKnots = 6;
+    int numKnots = 10;
     Double_t xKnots[numKnots];
     Double_t yKnots[numKnots];
     for(int iKnot=0; iKnot<numKnots; iKnot++){
@@ -95,6 +148,33 @@ double Spline3(double *x, double *par){
     }
     TSpline3* sp3 = new TSpline3("sp3", xKnots, yKnots, numKnots, "");
     return sp3->Eval(x[0]);
+}
+
+double Spline3Range(double *x, double *par){
+    int numKnots = 10;
+    Double_t xKnots[numKnots];
+    Double_t yKnots[numKnots];
+    for(int iKnot=0; iKnot<numKnots; iKnot++){
+        xKnots[iKnot] = par[iKnot];
+        yKnots[iKnot] = par[numKnots+iKnot];
+    }
+    TSpline3* sp3 = new TSpline3("sp3", xKnots, yKnots, numKnots, "");
+    
+    //if(x[0]<95 || x[0]>) return 0;
+    //else {
+    //    return sp3->Eval(x[0]);
+    //}
+    return sp3->Eval(x[0]);
+}
+
+double Spline3Histo(double *x, double *par){
+
+    TFile *histoFile = TFile::Open("/home/mdicostanzo/an/LPi/Analysis/SimAllMothersMerged.root", "r");
+    TH1D *splineHisto = static_cast<TH1D*>(histoFile->Get("Pairs/hSE_2113122_NoDirectSigmaXi_smearednew"));
+    TSpline3* sp3 = new TSpline3(splineHisto);
+    
+    return sp3->Eval(x[0]);
+
 }
 
 double Spline5(double *x, double *par){
@@ -108,3 +188,63 @@ double Spline5(double *x, double *par){
     TSpline5* sp5 = new TSpline5("sp5", xKnots, yKnots, numKnots, "");
     return sp5->Eval(x[0]);
 }
+
+ double Landau(double *x, double *par)
+ {
+    // par[0]: norm
+    // par[1]: mean
+    // par[2]: sigma
+
+    //if (par[2] <= 0) return 0;
+    //double den = ::ROOT::Math::landau_pdf( (x[0]-par[1])/par[2] );
+    //if (!par[0]) return den;
+    //return den/par[2];
+ 
+    if (par[2] <= 0) return 0;
+    double den = ::ROOT::Math::landau_pdf( (x[0]-par[1])/par[2] );
+    if (!par[0]) return den;
+    return par[0]*den;
+ 
+}
+
+//Double_t Sill_WithPS(Double_t *x, Double_t *par) {
+//
+//  // x[0]: k*
+//  // par: [0] "normalisation" constant
+//  //      [1] width
+//  //      [2] mass
+//  //      [3] resonance pT
+//  //      [4] kinetic decoupling temperature
+//
+//  Double_t LambdaFactor = 290;
+//
+//  if (x[0] < 0)
+//    return 0;
+//
+//  // double t = x[0];
+//
+//  Double_t kstar = x[0];
+//  Double_t Thresh = MassPion + MassProton;
+//
+//  double MotherMass = sqrt(kstar * kstar + MassPion * MassPion) + sqrt(kstar * kstar + MassProton * MassProton);
+//
+//  if (MotherMass < Thresh)
+//    return 0;
+//
+//  Double_t Width = par[1] * par[2] / sqrt(par[2] * par[2] - Thresh * Thresh);
+//  Double_t arg0 = 2 * MotherMass / TMath::Pi();
+//  Double_t arg1 = sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width;
+//  Double_t arg2 = pow(MotherMass * MotherMass - par[2] * par[2], 2.);
+//  Double_t arg3 = pow(sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width, 2.);
+//
+//  Double_t ResonancePt = par[3];
+//  Double_t Temperature = par[4]; // orignal paper 160
+//  Double_t PhaseSpace = (MotherMass / sqrt(MotherMass * MotherMass + ResonancePt * ResonancePt)) * exp(-sqrt(MotherMass * MotherMass + ResonancePt * ResonancePt) / Temperature);
+//  Double_t PhaseSpaceNorm = exp(-sqrt(Thresh * Thresh + ResonancePt * ResonancePt) / Temperature) * Temperature;
+//
+//  // return (par[0]*arg0*arg1/(arg2 + arg3))*PhaseSpace/(PhaseSpaceNorm*ME_Value); // *abs(Derivative_dM_dkStar(kstar));
+//
+//  return (par[0] * arg0 * arg1 / (arg2 + arg3)) * PhaseSpace / (PhaseSpaceNorm)*abs(Derivative_dM_dkStar(kstar));
+//
+//}
+
