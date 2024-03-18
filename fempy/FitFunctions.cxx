abd86b9329910952e0033ee9b97e81f078b4ed6b
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-03-18 13:02:46 +0100

    add ancestors
    add alice3 software options and remove .root extension
    
    remove .root extension from gitignore
    
    added fitting of subcomponents of fit terms
    
    added rejection range for Fitter
    
    changed canva size and deleted comments
    
    removed specific dpg drawopt
    
    deleted outdated files and comments
    
    removed comments and .root files
    
    deleted debug prints and Landau function
    
    deleted comment
    
    deleted duplicate of Lednicky function
    
    removed comments
    
    deleted other comments
    
    updated config file
    
    fix parenthesis

--- fempy/FitFunctions.cxx
+++ fempy/FitFunctions.cxx
@@ -1,16 +1,12 @@
 #include <map>
 #include <string>
 #include <tuple>
-<<<<<<< HEAD
 #include <complex>
-=======
->>>>>>> f60a00e (draw components in CF fitter)
 
 #include "TF1.h"
 #include "TFitResult.h"
 #include "TH1.h"
 #include "TMath.h"
-<<<<<<< HEAD
 #include "gsl/gsl_sf_dawson.h"
 
 double GlobNorm(double *x, double *par);
@@ -75,8 +71,6 @@ std::map<TString, std::tuple<double (*)(double *x, double *par), int>> functions
     {"sillkstar", {SillKStar, 3}}};
 
 double GlobNorm(double *x, double *par) { return 1.; }
-=======
->>>>>>> f60a00e (draw components in CF fitter)
 
 double Pol0(double *x, double *par) { return par[0]; }
 
@@ -90,7 +84,6 @@ double Pol4(double *x, double *par) { return Pol3(x, par) + par[4] * pow(x[0], 4
 
 double Pol5(double *x, double *par) { return Pol4(x, par) + par[5] * pow(x[0], 5); }
 
-<<<<<<< HEAD
 double FlatPol3(double *x, double *par) { 
     return 1 + par[4] * (Pol3(x, par) - 1); 
 }
@@ -148,8 +141,6 @@ double PowerLaw(double *x, double *par) {
     return par[0] + par[1] * TMath::Exp(-par[2] * x[0]) + Pol3(x, &par[3]); 
 }
 
-=======
->>>>>>> f60a00e (draw components in CF fitter)
 double Gaus(double *x, double *par) {
     double norm = 1. / TMath::Sqrt((2. * TMath::Pi())) / par[2];
     return norm * par[0] * TMath::Exp(-(x[0] - par[1]) * (x[0] - par[1]) / 2. / par[2] / par[2]);
@@ -189,13 +180,8 @@ double BreitWigner(double *x, double *par) {
     return yield * gamma / TMath::Pi() / (gamma * gamma + (kstar - mean) * (kstar - mean));
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-double BreitWignerKStar(double *x, double *par) {
-=======
 double BreitWignerKStar(double *x, double *par) {
 
->>>>>>> 1d86e56 (before baseline implementation)
   // p0: normalisation
   // p1: mass
   // p2: width
@@ -212,11 +198,6 @@ double BreitWignerKStar(double *x, double *par) {
 
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f60a00e (draw components in CF fitter)
-=======
 //double BreitWignerKStar(double *x, double *par) {
 //
 //  // x[0]: k*
@@ -252,9 +233,6 @@ double BreitWignerKStar(double *x, double *par) {
 //
 //}
 
->>>>>>> 1d86e56 (before baseline implementation)
-=======
->>>>>>> 2a4e774 (deleted comments)
 // Convolution of a Breit-Wigner and a gaussian
 double Voigt(double *x, double *par) {
     double kstar = x[0];
@@ -276,30 +254,17 @@ double Exp(double *x, double *par) {
 double PowEx(double *x, double *par) {
     // p0: total yield
     // p1: slope
-<<<<<<< HEAD
     double mpi = TDatabasePDG::Instance()->GetParticle(211)->Mass();
-=======
-    Double_t mpi = TDatabasePDG::Instance()->GetParticle(211)->Mass();
->>>>>>> f60a00e (draw components in CF fitter)
 
     if (x[0] < mpi) return 0;
     return par[0] * TMath::Sqrt(x[0] - mpi) * TMath::Exp(-1. * par[1] * (x[0] - mpi));
 }
 
 double Spline3(double *x, double *par){
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     int numKnots = 10;
-    double xKnots[numKnots];
-    double yKnots[numKnots];
-=======
-    int numKnots = 6;
-=======
-    int numKnots = 10;
->>>>>>> 1d86e56 (before baseline implementation)
     Double_t xKnots[numKnots];
     Double_t yKnots[numKnots];
->>>>>>> f60a00e (draw components in CF fitter)
     for(int iKnot=0; iKnot<numKnots; iKnot++){
         xKnots[iKnot] = par[iKnot];
         yKnots[iKnot] = par[numKnots+iKnot];
@@ -347,16 +312,10 @@ double Spline3Histo(double *x, double *par){
 
 }
 
-<<<<<<< HEAD
 double Spline5(double *x, double *par){
     int numKnots = 6;
-<<<<<<< HEAD
     double xKnots[numKnots];
     double yKnots[numKnots];
-=======
-    Double_t xKnots[numKnots];
-    Double_t yKnots[numKnots];
->>>>>>> f60a00e (draw components in CF fitter)
     for(int iKnot=0; iKnot<numKnots; iKnot++){
         xKnots[iKnot] = par[iKnot];
         yKnots[iKnot] = par[numKnots+iKnot];
@@ -364,8 +323,6 @@ double Spline5(double *x, double *par){
     TSpline5* sp5 = new TSpline5("sp5", xKnots, yKnots, numKnots, "");
     return sp5->Eval(x[0]);
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 double Spline3Range(double *x, double *par){
     int numKnots = 10;
@@ -473,12 +430,7 @@ double ComplexLednicky_Singlet_doublegaussian_lambda(double *x, double *par)
     return sourcePar3 * (sourcePar2 * GeneralLednicky(kStar, sourcePar0, ScatLen, potPar2) 
            + (1 - sourcePar2) * GeneralLednicky(kStar, sourcePar1, ScatLen, potPar2)) + 1. - sourcePar3;
 }
-=======
->>>>>>> f60a00e (draw components in CF fitter)
-=======
 
-=======
->>>>>>> 2a4e774 (deleted comments)
  double Landau(double *x, double *par)
  {
     // par[0]: norm
@@ -496,46 +448,3 @@ double ComplexLednicky_Singlet_doublegaussian_lambda(double *x, double *par)
     return par[0]*den;
  
 }
-
-//Double_t Sill_WithPS(Double_t *x, Double_t *par) {
-//
-//  // x[0]: k*
-//  // par: [0] "normalisation" constant
-//  //      [1] width
-//  //      [2] mass
-//  //      [3] resonance pT
-//  //      [4] kinetic decoupling temperature
-//
-//  Double_t LambdaFactor = 290;
-//
-//  if (x[0] < 0)
-//    return 0;
-//
-//  // double t = x[0];
-//
-//  Double_t kstar = x[0];
-//  Double_t Thresh = MassPion + MassProton;
-//
-//  double MotherMass = sqrt(kstar * kstar + MassPion * MassPion) + sqrt(kstar * kstar + MassProton * MassProton);
-//
-//  if (MotherMass < Thresh)
-//    return 0;
-//
-//  Double_t Width = par[1] * par[2] / sqrt(par[2] * par[2] - Thresh * Thresh);
-//  Double_t arg0 = 2 * MotherMass / TMath::Pi();
-//  Double_t arg1 = sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width;
-//  Double_t arg2 = pow(MotherMass * MotherMass - par[2] * par[2], 2.);
-//  Double_t arg3 = pow(sqrt(MotherMass * MotherMass - Thresh * Thresh) * Width, 2.);
-//
-//  Double_t ResonancePt = par[3];
-//  Double_t Temperature = par[4]; // orignal paper 160
-//  Double_t PhaseSpace = (MotherMass / sqrt(MotherMass * MotherMass + ResonancePt * ResonancePt)) * exp(-sqrt(MotherMass * MotherMass + ResonancePt * ResonancePt) / Temperature);
-//  Double_t PhaseSpaceNorm = exp(-sqrt(Thresh * Thresh + ResonancePt * ResonancePt) / Temperature) * Temperature;
-//
-//  // return (par[0]*arg0*arg1/(arg2 + arg3))*PhaseSpace/(PhaseSpaceNorm*ME_Value); // *abs(Derivative_dM_dkStar(kstar));
-//
-//  return (par[0] * arg0 * arg1 / (arg2 + arg3)) * PhaseSpace / (PhaseSpaceNorm)*abs(Derivative_dM_dkStar(kstar));
-//
-//}
-
->>>>>>> 1d86e56 (before baseline implementation)
