08c125d566083eb28f602caf02d90c870edbc32a
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-03-21 23:22:52 +0100

    added list of lines to avoid error

--- CompareGraphs.py
+++ CompareGraphs.py
@@ -112,6 +112,7 @@ for plot in cfg:
                 legend += f';  #sigma={inObj.GetStdDev():.3f}'
         leg.AddEntry(inObj, legend, 'lp')
         
+    inputlines = []
     for line in plot['opt']['lines']:
         x1 = plot['opt']['rangex'][0] if(line['coordinates'][0] == 'min') else line['coordinates'][0]
         y1 = plot['opt']['rangey'][0] if(line['coordinates'][1] == 'min') else line['coordinates'][1]
@@ -121,6 +122,7 @@ for plot in cfg:
         inputline.SetLineColor(style.GetColor(line['color']))
         inputline.SetLineWidth(line['thickness'])
         inputline.Draw("same")
+        inputlines.append(inputline)
         if('legendtag' in line):
             leg.AddEntry(inputline, TranslateToLatex(line['legendtag']),"l")
         
