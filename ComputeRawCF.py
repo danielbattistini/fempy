449bb3112a8e2c80c23b9a1cd803d0af6339a38a
Marcellocosti <marcellodicostanzo00@gmail.com>
2024-06-09 17:38:48 +0200

    save reweighted ME

--- ComputeRawCF.py
+++ ComputeRawCF.py
@@ -213,6 +213,7 @@ for comb in combs + ['p02_13', 'p03_12']:
         
         hMErew[comb][region].SetName('hMErew')
         hMErew[comb][region].SetTitle(';#it{k}* (GeV/#it{c});Counts')
+        hMErew[comb][region].Write()
 
         hWeightsRew[comb][region].SetName('hWeightsRew')
         hWeightsRew[comb][region].SetTitle(';Mult bin;Weight')
